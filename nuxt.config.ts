87e0a6bbd61cb084b5ed3e63b4e34d7b1dc45b0c
Patrick Shushereba <pshushereba@gmail.com>
2023-01-22 14:37:40 -0800

    Updated nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  build: {
+    transpile: ['@heroicons/vue'],
+  },
   modules: ['@nuxt/content'],
   css: ['~/assets/css/main.css'],
   postcss: {
