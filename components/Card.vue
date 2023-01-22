10898b844f125f64e82b88fd0e325adac701fb2a
Patrick Shushereba <pshushereba@gmail.com>
2023-01-21 19:27:51 -0800

    Rough draft of Card component.

---
+++ components/Card.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="group relative flex flex-col items-start">
+    <h2 class="text-base font-semibold tracking-tight text-zinc-800 dark:text-zinc-100">
+      Crafting a design system for a multiplanetary future
+      <div
+        class="absolute -inset-y-6 -inset-x-4 z-0 scale-95 bg-zinc-50 opacity-0 transition group-hover:scale-100 group-hover:opacity-100 dark:bg-zinc-800/50 sm:-inset-x-6 sm:rounded-2xl"
+      />
+      <NuxtLink>
+        <span class="absolute -inset-y-6 -inset-x-4 z-20 sm:-inset-x-6 sm:rounded-2xl"></span>
+        <span class="relative z-10"></span>
+      </NuxtLink>
+    </h2>
+    <p
+      class="relative z-10 order-first mb-3 flex items-center text-sm text-zinc-400 dark:text-zinc-500"
+    >
+      September 5, 2022
+    </p>
+    <p class="relative z-10 mt-2 text-sm text-zinc-600 dark:text-zinc-400">
+      Most companies try to stay ahead of the curve when it comes to visual design, but for
+      Planetaria we needed to create a brand that would still inspire us 100 years from now when
+      humanity has spread across our entire solar system.
+    </p>
+    <div class="relative z-10 mt-4 flex items-center text-sm font-medium text-teal-500">
+      Read Article
+      <svg viewBox="0 0 16 16" fill="none" aria-hidden="true" class="ml-1 h-4 w-4 stroke-current">
+        <path
+          d="M6.75 5.75 9.25 8l-2.5 2.25"
+          strokeWidth="1.5"
+          strokeLinecap="round"
+          strokeLinejoin="round"
+        />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -45,9 +45,9 @@
       <Container class="mt-24 md:mt-28">
         <div class="mx-auto grid max-w-xl grid-cols-1 gap-y-20 lg:max-w-none lg:grid-cols-2">
           <div class="flex flex-col gap-16">
-            <!-- {articles.map((article) => (
-                <Article key={article.slug} article={article} />
-              ))} -->
+            <Card></Card>
+            <Card></Card>
+            <Card></Card>
           </div>
           <div class="space-y-10 lg:pl-16 xl:pl-24">
             <!-- <Newsletter />
