003ff61d06cec337fbf849fa646d8975fe16170b
Patrick Shushereba <pshushereba@gmail.com>
2023-01-22 14:41:28 -0800

    Updated meta tag.

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <title>Patrick Shushereba - Software Developer, founder, and amateur astronaut</title>
+    <title>Patrick Shushereba - Software Developer, founder, and amateur runner</title>
     <meta
       name="description"
       content="I'm Spencer, a software designer and entrepreneur based in New York City. I'm the founder and CEO of Planetaria, where we develop technologies that empower regular people to explore space on their own terms."
